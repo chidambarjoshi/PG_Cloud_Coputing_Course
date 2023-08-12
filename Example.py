ccafd4b3a04d345a1d515872030fdb5c3a2aaa41
Chidambar Joshi <35500446+chidambarjoshi@users.noreply.github.com>
2023-08-12 22:52:10 +0530

    Changes in file structure

--- Example.py
+++
@@ -1,6 +0,0 @@
-import Module as m 
-
-print(m.addition(4,6))
-print(m.substraction(4,5))
-print(m.multiplication(1,3))
-print(m.division(4,2))
---
+++ Python/Example.py
@@ -0,0 +1,6 @@
+import Module as m 
+
+print(m.addition(4,6))
+print(m.substraction(4,5))
+print(m.multiplication(1,3))
+print(m.division(4,2))
